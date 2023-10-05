f92d0542e2394755aba55c9b077ec2a594342fda
LONOAL <lnogueirapan@danielcastelao.org>
2023-10-05 14:04:57 +0200

    sisisisidusfusdkf

--- app/src/main/java/com/dam/riotapitest/MainActivity.kt
+++ app/src/main/java/com/dam/riotapitest/MainActivity.kt
@@ -1,80 +1,36 @@
 package com.dam.riotapitest
 
+import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dam.riotapitest.ui.theme.RiotApiTestTheme
-import groovy.swing.factory.ComboBoxFactory
-import java.io.File
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         Api().initApi()
-        Api().getMasteryPoints("LONOAL02", "Swain", this,2) { masteryPoints ->
-            Log.d("Hola", "Puntos de Maestría: $masteryPoints")
-        }
+        ui()
 
-        setContent {
-            RiotApiTestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting(applicationContext)
-                }
+    }
+
+
+    fun ui (){
+        button2.setOnClickListener {
+            Api().getMasteryPoints(editText.text.toString(), editText2.text.toString(), this) { masteryPoints ->
+                textView3.text = textView3.text.toString()+masteryPoints.toString()
             }
         }
-    }
-
-
-}
-
-
-
-@Composable
-fun Greeting(context : Context) {
-    var sumName = remember {
-        mutableStateOf("")
-    }
-    var champName = remember {
-        mutableStateOf("")
-    }
-    var points = remember {
-        mutableStateOf(0)
-    }
-    var serverSelect = remember{ mutableStateOf(0) }
-
-    Column(Modifier.padding(16.dp)){
-        TextField(value = sumName.value, onValueChange = { sumName.value = it }, Modifier.padding(16.dp))
-        TextField(value = champName.value, onValueChange = { champName.value = it }, Modifier.padding(16.dp))
-        DropdownMenu(expanded = false, onDismissRequest = {
-        }) {
-            DropdownMenuItem( onClick = { serverSelect.value = 1 }) {}
-            DropdownMenuItem( onClick = { serverSelect.value = 2 }) {}
-            DropdownMenuItem( onClick = { serverSelect.value = 3 }) {}
-        }
-        Button(onClick = { Api().getMasteryPoints(sumName.value,champName.value, context ,serverSelect.value){ masteryPoints ->
-            points.value=masteryPoints
-        }
-        }) {
-
-        }
-        Text(text = points.value.toString())
     }
 
 }
